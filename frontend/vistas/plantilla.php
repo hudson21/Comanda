eaff59fe5be5759f24b0ad663b52aea8d8c0bd96
Trainee IT <trainee.saim@excellence.local>
2018-01-24 13:30:58 -0500

    Haciendo el cabezote en el frontend

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -5,9 +5,40 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="title" content="Comanda Electrónica">
 	<meta name="description" content="Esta es una página para un proyecto de residencia bla bla bla">
+	<meta name="keyword" content="tienda, vinos, licores,etc">
+
+	<link rel="stylesheet" href="css/plugins/bootstrap.min.css">
+	<link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+
+	<script src="js/plugins/bootstrap.min.js"></script>
+	<script src="js/plugins/jquery.min.js"></script>
 	<title>Comanda Electrónica</title>
 </head>
 <body>
-	<h1>Título</h1>
+	
+
+	<!--Para generar comentarios va a ser con 
+  
+  				comm-html-section
+	-->
+
+	<!--=====================================
+	=            Section comment            =
+	======================================-->
+	
+	
+	
+	<!--====  End of Section comment  ====-->
+
+<?php
+  /*=============================================
+  		=            CABEZOTE            =
+  =============================================*/
+  include "modulos/cabezote.php";
+  
+  
+  
+?>
+	
 </body>
 </html>
