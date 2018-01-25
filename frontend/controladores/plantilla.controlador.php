bcca7abf5dff60d137a345c5785204d79105be58
Trainee IT <trainee.saim@excellence.local>
2018-01-25 16:55:48 -0500

    Commit de la conexion de la base de datos

--- frontend/controladores/plantilla.controlador.php
+++ frontend/controladores/plantilla.controlador.php
@@ -2,8 +2,23 @@
 
 class ControladorPlantilla{//Aquí tenemos la clase ControladorPlantilla
 
+	/*-------------------------------------
+	LLAMAMOS A LA PLANTILLA
+	---------------------------------------*/
+
 	public function plantilla(){//Creamos una función pública para poder hacer uso de esta en otras clases
 
 		include "vistas/plantilla.php"; //Estamos incluyendo la plantilla que creamos en vistas
 	}
+
+	/*----------------------------------------------
+	  TRAEMOS LOS ESTILOS DINÁMICOS DE LA PLANTILLA
+	  ----------------------------------------------*/
+
+	  public function ctrEstiloPlantillaI(){
+
+	  	$tabla = "plantilla";
+
+	  	$respuesta = ModeloPlantilla::mdlEstiloPlantilla($tabla);
+	  }
 }
