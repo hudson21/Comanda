9e246b33723060b938ae195b5a48a642324e9393
Trainee IT <trainee.saim@excellence.local>
2018-01-25 16:11:55 -0500

    Agregué el archivo sql de la base de datos donde está la tabla plantilla con su respectivo JSON

---
+++ comanda_electronica.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-01-2018 a las 22:10:34
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `comanda_electronica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plantilla`
+--
+
+CREATE TABLE `plantilla` (
+  `id` int(11) NOT NULL,
+  `barraSuperior` text COLLATE utf8_spanish_ci NOT NULL,
+  `textoSuperior` text COLLATE utf8_spanish_ci NOT NULL,
+  `colorFondo` text COLLATE utf8_spanish_ci NOT NULL,
+  `colorTexto` text COLLATE utf8_spanish_ci NOT NULL,
+  `logo` text COLLATE utf8_spanish_ci NOT NULL,
+  `icono` text COLLATE utf8_spanish_ci NOT NULL,
+  `redesSociales` text COLLATE utf8_spanish_ci NOT NULL,
+  `fecha` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `plantilla`
+--
+
+INSERT INTO `plantilla` (`id`, `barraSuperior`, `textoSuperior`, `colorFondo`, `colorTexto`, `logo`, `icono`, `redesSociales`, `fecha`) VALUES
+(1, '#000000', '#ffffff', '#C1CA2C', '#ffffff', 'vistas/img/plantilla/logo.png', 'vistas/img/plantilla/icono.png', '[\r\n\r\n	{\r\n		\"red\": \"fa-facebook\",\r\n		\"estilo\": \"facebookBlanco\",\r\n		\"url\": \"http://facebook.com/\"\r\n	},\r\n\r\n	{\r\n		\"red\": \"fa-youtube\",\r\n		\"estilo\": \"youtubeBlanco\",\r\n		\"url\": \"http://youtube.com\"\r\n	},\r\n\r\n	{\r\n		\"red\": \"fa-twitter\",\r\n		\"estilo\": \"twitterBlanco\",\r\n		\"url\": \"http://twitter.com/\"\r\n	},\r\n\r\n	{\r\n		\"red\": \"fa-google-plus\",\r\n		\"estilo\": \"googleBlanco\",\r\n		\"url\": \"http://google.com/\"\r\n	},\r\n\r\n	{\r\n		\"red\": \"fa-instagram\",\r\n		\"estilo\": \"instagramBlanco\",\r\n		\"url\": \"http://instagram.com/\"\r\n	}\r\n]', '2018-01-25 21:04:31');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `plantilla`
+--
+ALTER TABLE `plantilla`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `plantilla`
+--
+ALTER TABLE `plantilla`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
