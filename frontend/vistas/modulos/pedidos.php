a61f11d3b15c444df32901579a2d4148993da11f
Trainee IT <trainee.saim@excellence.local>
2018-03-16 15:20:40 -0500

    Actualizando la base de datos

--- frontend/vistas/modulos/pedidos.php
+++ frontend/vistas/modulos/pedidos.php
@@ -443,65 +443,6 @@ if(!isset($_SESSION["validarSesion"])){
 </div>
 </div>
 
-<br><br><br>
-
-<h2>Este es el de ejemplo no te confundas</h2>
-<div class="panel-group" id="accordion">
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        Collapsible Group 1</a>
-      </h4>
-    </div>
-    <div id="collapse1" class="panel-collapse collapse ">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-        Collapsible Group 2</a>
-      </h4>
-    </div>
-    <div id="collapse2" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-
-      asffafadfasfdsafdsafsadfasfdsafsafsafdsafdsafdsafdsafdsaf
-
-      <div>sadñ{sldkf{ñalskfd{ñsalkfñ{askfdñ{salkfd</div>
-   </div>
-    </div>
-  </div>
-  <div class="panel panel-default">
-    <div class="panel-heading">
-      <h4 class="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-        Collapsible Group 3</a>
-      </h4>
-    </div>
-    <div id="collapse3" class="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-
-      asffafadfasfdsafdsafsadfasfdsafsafsafdsafdsafdsafdsafdsaf
-
-      <div>sadñ{sldkf{ñalskfd{ñsalkfñ{askfdñ{salkfd</div>
-   </div>
-    </div>
-  </div>
-
-  
-</div>
 
 
 					
